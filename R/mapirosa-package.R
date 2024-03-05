ccad385beaa6ed59815e0b3577c9cc78c7e0ff46
Fran Barton <francis.barton@nuh.nhs.uk>
2024-03-05 19:05:46 +0000

    Update package documentation

--- R/mapirosa-package.R
+++ R/mapirosa-package.R
@@ -2,10 +2,12 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom usethis ui_info ui_yeah
+#' @importFrom usethis ui_info ui_nope
 #' @importFrom assertthat assert_that
-#' @importFrom httr2 request req_user_agent req_url_path_append req_url_query
-#' @importFrom httr2 req_perform resp_body_raw resp_body_xml resp_check_status
+#' @importFrom httr2 request req_perform
+#' @importFrom httr2 req_user_agent req_url_path_append req_url_query
+#' @importFrom httr2 resp_body_json resp_body_raw resp_body_xml 
+#' @importFrom httr2 resp_check_status
 #' @importFrom rlang .data
 ## usethis namespace: end
 NULL
